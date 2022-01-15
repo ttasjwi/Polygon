d5360d741b0a597472c313052e7709f3e10580c0
ttasjwi <ttasjwi920@gmail.com>
2022-01-15 21:27:51 +0900

    Point 클래스 - x,y 좌표로 정의하도록 변경.
    필요에 따라 위치벡터를 좌표 기반으로  꺼내 쓸 수 있도록 함.

--- Point.java
+++ Point.java
@@ -2,15 +2,21 @@ import java.util.Objects;
 
 public class Point {
 
-    private Vector position;
+    private int x;
+    private int y;
 
     public Point(int x, int y) {
-        this.position = Vector.of(x,y);
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector positionVector() {
+        return Vector.of(this.x, this.y);
     }
 
     // 두 점 사이의 거리
     public static double distance(Point p1, Point p2) {
-        Vector subtract = Vector.subtract(p1.position, p2.position);
+        Vector subtract = Vector.subtract(p1.positionVector(), p2.positionVector());
         return subtract.size(); // 벡터의 차를 구하고, 그 크기를 반환
     }
 
@@ -24,12 +30,17 @@ public class Point {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return position.equals(point.position);
+        return x == point.x && y == point.y;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(position);
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d,%d)",x,y);
     }
 
 }
